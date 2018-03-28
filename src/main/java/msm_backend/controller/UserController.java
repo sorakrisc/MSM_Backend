3652ad1ac2b06e1eb939cd99fbf55c2b0347c5fe
James Sorakris <csorakris@gmail.com>
2018-03-28 23:04:02 +0700

    change back to the first version of add courses list of course

--- src/main/java/msm_backend/controller/UserController.java
+++ src/main/java/msm_backend/controller/UserController.java
@@ -149,11 +149,11 @@ public class UserController {
     }
     @PostMapping("/addCoursesToPlan")
     @ResponseBody
-    void addCoursesToPlan(@RequestBody() Map<String,Map<String,String>> courses,
+    void addCoursesToPlan(@RequestBody() List<Course> courses,
                           @RequestParam("planname") String planname, Authentication auth ){
-        for(Map.Entry<String, Map<String, String>> courseMap: courses.entrySet()){
-            Map<String, String> course =courseMap.getValue();
-            addCourseToPlanHelper(course.get("courseskyid"),planname, auth);
+        for(Course course:courses){
+            System.out.println(course.getSkyid());
+            addCourseToPlanHelper(course.getSkyid(),planname, auth);
         }
     }
 
